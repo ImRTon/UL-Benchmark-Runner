8c60f5068c640afe87fd9ab6010d6a3ef942246c
RTon <50702495+ImRTon@users.noreply.github.com>
2025-01-09 22:47:04 +0800

    feat: better handle windows chaos path system

--- README.md
+++ README.md
@@ -3,5 +3,12 @@ This is a simple benchmark runner for the UL Benchmarks, ex: Procyon, 3DMark, PC
 
 ## Usage
 ```bash
-python main.py -i <UL Parent Dir Path> -d <Device Name> -o <Output Dir Path>
+python main.py -i <UL Parent Dir Path> -d <Device Name> -o <Output Dir Path> --ul_3dmark --ul_procyon --shutdown
 ```
+* `-i` or `--input` : Path to the UL Parent Directory. The path must be enclosed in double quotes (e.g., "C:\Your Path")  
+* `-d` or `--device` : Device Name  
+* `-o` or `--output` : Output Directory Path  
+* `-s` or `--sleep` : Idel time in seconds between benchmarks
+* `--ul_3dmark` : Run 3DMark Benchmark  
+* `--ul_procyon` : Run Procyon Benchmark  
+* `--shutdown` : Shutdown the system after the benchmark is completed  
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-import os
+import subprocess
 import time
 
 from pathlib import Path
@@ -11,6 +11,9 @@ def get_args():
     parser.add_argument('-i', '--dir_path', type=str, required=True, help='Benchmarks parent directory.')
     parser.add_argument('-d', '--device', type=str, required=True, help='cuda or xpu')
     parser.add_argument('-o', '--output', type=str, required=True, help='Output dir path')
+    parser.add_argument('-s', '--sleep', type=int, default=5*60, help='Sleep time between benchmarks')
+    parser.add_argument('--ul_3dmark', action='store_true', help='Run 3DMark benchmarks')
+    parser.add_argument('--ul_procyon', action='store_true', help='Run Procyon benchmarks')
     parser.add_argument('--shutdown', action='store_true', help='Shutdown the computer after complete the benchmarks')
     return parser.parse_args()
 
@@ -19,20 +22,33 @@ def main():
     output_dir = Path(args.output)
 
     # 3DMark
-    for benchmark_name in ul_3dmarks:
-        output_path = output_dir / f"{benchmark_name.split('.')[0]}.3dmark-result"
-        xml_output_path = output_dir / f"{benchmark_name.split('.')[0]}.xml"
-        os.system(f'{args.dir_path}\\3DMark\\3DMarkCmd.exe --definition={benchmark_name} '
-                  f'--result="{output_path.absolute()}" --export="{xml_output_path.absolute()}"')
-        time.sleep(5*60)
+    if args.ul_3dmark:
+        for benchmark_name in ul_3dmarks:
+            output_path = output_dir / f"{benchmark_name.split('.')[0]}.3dmark-result"
+            xml_output_path = output_dir / f"{benchmark_name.split('.')[0]}.xml"
+
+            subprocess.run([
+                f'{args.dir_path}\\3DMark\\3DMarkCmd.exe', 
+                f'--definition={benchmark_name}', 
+                f'--out={output_path.absolute()}', 
+                f'--export={xml_output_path.absolute()}'
+            ])
+
+            time.sleep(args.sleep)
 
     # Procyon
-    for benchmark_name in ul_procyons[args.device]:
-        output_path = output_dir / f"{benchmark_name.split('/')[-1].split('.')[0]}.procyon-result"
-        csv_output_path = output_dir / f"{benchmark_name.split('/')[-1].split('.')[0]}.csv"
-        os.system(f'{args.dir_path}\\Procyon\\ProcyonCmd.exe --definition={benchmark_name} '
-                  f'--result="{output_path.absolute()}" --export-csv "{csv_output_path.absolute()}"')
-        time.sleep(5*60)
+    if args.ul_procyon:
+        for benchmark_name in ul_procyons[args.device]:
+            output_path = output_dir / f"{benchmark_name.split('/')[-1].split('.')[0]}.procyon-result"
+            csv_output_path = output_dir / f"{benchmark_name.split('/')[-1].split('.')[0]}.csv"
+
+            subprocess.run([
+                f'{args.dir_path}\\Procyon\\ProcyonCmd.exe', 
+                f'--definition={Path.cwd().absolute()}\\{benchmark_name}', 
+                f'--result="{output_path.absolute()}"', 
+                f'--export-csv="{csv_output_path.absolute()}"'
+            ])
+            time.sleep(args.sleep)
 
     if args.shutdown:
         os.system("shutdown /s /t 1") # Shutdown the computer
